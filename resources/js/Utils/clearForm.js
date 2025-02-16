09c45a86d48f73baad0af4d3e7daeb4873c1ea87
Luiz Fernando <cruzfernando.dev@gmail.com>
2025-02-16 15:51:33 -0300

    Refactor clearForm utility function to use hasOwnProperty for default value assignment

--- resources/js/Utils/clearForm.js
+++ resources/js/Utils/clearForm.js
@@ -1,5 +1,5 @@
 export function clearForm(form, defaultValues = {}) {
     Object.keys(form.value).forEach((key) => {
-        form.value[key] = defaultValues[key] || "";
+        form.value[key] = defaultValues.hasOwnProperty(key) ? defaultValues[key] : "";
     });
 }
