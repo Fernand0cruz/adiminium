b26d4260173ef140d4f9f80abcc0b8418a397e82
Luiz Fernando <cruzfernando.dev@gmail.com>
2025-02-18 18:15:11 -0300

    Add FormErrorInput component to display error messages

---
+++ resources/js/Components/FormErrorInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-show="props.message">
+        <p class="text-sm text-red-600">
+            {{ props.message }}
+        </p>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    message: String
+});
+</script>
