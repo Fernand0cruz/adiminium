bbac630f16ed646b9d75f6e5a2fb11a2cd37db6c
Luiz Fernando <cruzfernando.dev@gmail.com>
2025-02-21 16:57:35 -0300

    Add product description to product listing and update table headers

--- resources/js/Components/TableProducts.vue
+++ resources/js/Components/TableProducts.vue
@@ -7,6 +7,8 @@
 
                     <TableHeader colTitle="Nome" />
 
+                    <TableHeader colTitle="Descricao" />
+
                     <TableHeader colTitle="Preço Base" />
 
                     <TableHeader colTitle="Desconto" />
@@ -15,11 +17,11 @@
 
                     <TableHeader colTitle="Quantidade" />
 
-                    <th></th>
+                    <TableHeader colTitle="Ver" />
 
-                    <th></th>
+                    <TableHeader colTitle="Editar" />
 
-                    <th></th>
+                    <TableHeader colTitle="Excluir" />
                 </tr>
             </thead>
 
@@ -37,6 +39,17 @@
                     </td>
 
                     <TableData :data="product.name" />
+
+                    <td class="size-px whitespace-nowrap">
+                        <span class="block px-6 py-2">
+                            <span
+                                class="text-sm text-gray-600 dark:text-neutral-400 overflow-hidden text-ellipsis whitespace-nowrap block w-80"
+                                :title="product.description"
+                            >
+                                {{ product.description }}
+                            </span>
+                        </span>
+                    </td>
 
                     <TableData :data="product.price" />
 
@@ -64,30 +77,27 @@
                             :href="
                                 route('admin.products.show', { id: product.id })
                             "
-                            class="py-1 px-2 inline-flex justify-center items-center gap-2 rounded-lg border border-indigo-500 font-medium bg-indigo-100 text-indigo-700 align-middle hover:bg-indigo-200 transition-all text-sm"
+                            class="py-1 px-2 mx-6 inline-flex justify-center items-center gap-2 rounded-lg border border-indigo-500 font-medium bg-indigo-100 text-indigo-700 align-middle hover:bg-indigo-200 transition-all text-sm"
                         >
                             <ExternalLink />
-                            Visualizar
                         </Link>
                     </td>
 
                     <td class="size-px whitespace-nowrap">
                         <Link
                             href=""
-                            class="py-1 px-2 inline-flex justify-center items-center gap-2 rounded-lg border border-green-500 font-medium bg-green-100 text-green-700 align-middle hover:bg-green-200 transition-all text-sm"
+                            class="py-1 px-2 mx-6 inline-flex justify-center items-center gap-2 rounded-lg border border-green-500 font-medium bg-green-100 text-green-700 align-middle hover:bg-green-200 transition-all text-sm"
                         >
                             <Clipboard />
-                            Editar
                         </Link>
                     </td>
 
                     <td class="size-px whitespace-nowrap">
                         <Link
                             href=""
-                            class="py-1 px-2 inline-flex justify-center items-center gap-2 rounded-lg border border-red-500 font-medium bg-red-100 text-red-700 align-middle hover:bg-red-200 transition-all text-sm"
+                            class="py-1 px-2 mx-6 inline-flex justify-center items-center gap-2 rounded-lg border border-red-500 font-medium bg-red-100 text-red-700 align-middle hover:bg-red-200 transition-all text-sm"
                         >
                             <Trash />
-                            Excluir
                         </Link>
                     </td>
                 </tr>
