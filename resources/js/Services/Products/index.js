5c3b056b4b010a3984f3468abafdddf76b102889
Luiz Fernando <cruzfernando.dev@gmail.com>
2025-03-02 17:22:36 -0300

    Refactor product service; consolidate create and update logic, implement centralized error handling

--- resources/js/Services/Products/index.js
+++ resources/js/Services/Products/index.js
@@ -1,80 +1,50 @@
 import { formatCurrency } from "@/Utils/formatCurrency";
+import { handleRequestError } from "@/Utils/handleRequestError";
+
+const createOrUpdateProduct = (productData, method) => {
+    const formData = new FormData();
+    formData.append("photo", productData.photo);
+    formData.append("name", productData.name);
+    formData.append("description", productData.description);
+    formData.append("price", productData.price);
+    const discount = productData.discount ? parseFloat(productData.discount) : 0;
+    formData.append("discount", discount);
+    formData.append("quantity", productData.quantity);
+
+    if (method === "PATCH") {
+        formData.append("_method", "PATCH");
+    }
+
+    return formData; 
+};
+
+const formatProductData = (product) => {
+    const price = parseFloat(product.price);
+    const discount = parseFloat(product.discount);
+    const finalPrice = price - price * (discount / 100);
+
+    return {
+        ...product,
+        price: formatCurrency(price),
+        discount: discount.toFixed(2),
+        final_price: formatCurrency(finalPrice),
+    };
+};
 
 export default (httpClient) => ({
     create: async (productData) => {
         try {
-            const formData = new FormData();
-            formData.append("photo", productData.photo);
-            formData.append("name", productData.name);
-            formData.append("description", productData.description);
-            formData.append("price", productData.price);
-            const discount = productData.discount ? parseFloat(productData.discount) : 0;
-            formData.append("discount", discount);
-            formData.append("quantity", productData.quantity);
-
-            const response = await httpClient.post("/api/products", formData, {
-                headers: { "Content-Type": "multipart/form-data" },
-            });
-
+            const data = createOrUpdateProduct(productData) 
+            const response = await httpClient.post("/api/products", data);
             return response.data;
         } catch (error) {
-            if (error.response) {
-                if (error.response.status === 422) {
-                    console.warn(
-                        "Erro de validação:",
-                        error.response.data.errors
-                    );
-                    throw error.response.data.errors;
-                }
-
-                if (error.response.status === 413) {
-                    console.warn("Erro: Arquivo muito grande.");
-                    throw { photo: ["O arquivo de imagem é muito grande."] };
-                }
-
-                console.error(
-                    "Erro do servidor:",
-                    error.response.status,
-                    error.response.data
-                );
-                throw {
-                    message: [
-                        "Ocorreu um erro no servidor. Tente novamente mais tarde!",
-                    ],
-                };
-            }
-
-            if (error.message === "Network Error") {
-                console.error("Erro de rede. O backend pode estar offline.");
-                throw {
-                    message: [
-                        "Erro de conexão com o servidor. Verifique sua internet ou tente novamente mais tarde!",
-                    ],
-                };
-            }
-
-            console.error("Erro desconhecido:", error);
-            throw { message: ["Ocorreu um erro inesperado."] };
+            return handleRequestError(error);
         }
     },
-
     getAll: async (page = 1) => {
         try {
             const response = await httpClient.get(`/api/products?page=${page}`);
-
-            const formattedData = response.data.data.data.map((product) => {
-                const price = parseFloat(product.price);
-                const discount = parseFloat(product.discount);
-                const finalPrice = price - price * (discount / 100);
-
-                return {
-                    ...product,
-                    price: formatCurrency(price),
-                    discount: discount.toFixed(2),
-                    final_price: formatCurrency(finalPrice),
-                };
-            });
-
+            const formattedData = response.data.data.data.map(formatProductData);
             return {
                 ...response.data,
                 data: {
@@ -83,28 +53,33 @@ export default (httpClient) => ({
                 },
             };
         } catch (error) {
-            console.error("Erro ao buscar produtos:", error);
-            throw error;
+            return handleRequestError(error);
         }
     },
     get: async (productId) => {
         try {
             const response = await httpClient.get(`/api/products/${productId}`);
-
-            const product = response.data.data;
-            const price = parseFloat(product.price);
-            const discount = parseFloat(product.discount);
-            const finalPrice = price - price * (discount / 100);
-
-            return {
-                ...product,
-                price: formatCurrency(price),
-                discount: discount.toFixed(2),
-                final_price: formatCurrency(finalPrice),
-            };
+            const product = formatProductData(response.data.data);
+            return product
         } catch (error) {
-            console.error("Erro ao buscar produto:", error);
-            throw error;
+            return handleRequestError(error);
+        }
+    },
+    update: async (productId, productData) => {
+        try {
+            const data = createOrUpdateProduct(productData, "PATCH") 
+            const response = await httpClient.post(`/api/products/${productId}`, data);
+            return response.data;
+        } catch (error) {
+            return handleRequestError(error);
+        }
+    },
+    delete: async (productId) => {
+        try {
+            const response = await httpClient.delete(`/api/products/${productId}`);
+            return response.data;
+        } catch (error) {
+            return handleRequestError(error);
         }
     },
 });
---
+++ resources/js/Utils/handleRequestError.js
@@ -0,0 +1,33 @@
+export function handleRequestError(error) {
+    if (error.response) {
+        if (error.response.status === 422) {
+            console.warn("Erro de validação:", error.response.data.errors);
+            throw error.response.data.errors;
+        }
+
+        if (error.response.status === 413) {
+            console.warn("Erro: Arquivo muito grande.");
+            throw { photo: ["O arquivo de imagem é muito grande."] };
+        }
+
+        if (error.response.status === 404) {
+            console.warn("Erro: Dado(s) não encontrado.");
+            throw { message: ["Dado(s) não encontrado no sistema."] };
+        }
+
+        console.error("Erro do servidor:", error.response.status, error.response.data);
+        throw {
+            message: ["Ocorreu um erro no servidor. Tente novamente mais tarde!"],
+        };
+    }
+
+    if (error.message === "Network Error") {
+        console.error("Erro de rede. O backend pode estar offline.");
+        throw {
+            message: ["Erro de conexão com o servidor. Verifique sua internet ou tente novamente mais tarde!"],
+        };
+    }
+
+    console.error("Erro desconhecido:", error);
+    throw { message: ["Ocorreu um erro inesperado."] };
+}
