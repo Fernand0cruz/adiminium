da6463c019e486027495134dc8a1f561c86a4184
Luiz Fernando <cruzfernando.dev@gmail.com>
2025-03-02 17:26:45 -0300

    Refactor product form; streamline field handling with dynamic components and improve form submission logic

--- resources/js/Components/ProductForm.vue
+++ resources/js/Components/ProductForm.vue
@@ -4,7 +4,6 @@
 
     <form @submit.prevent="createOrUpdateProduct">
         <div class="grid sm:grid-cols-12 gap-2 sm:gap-6">
-            <!-- PHOTO PRODUCT -->
             <div class="sm:col-span-3">
                 <FormLabel for="photo" label="Foto do Produto:" />
             </div>
@@ -17,80 +16,28 @@
                 />
                 <FormErrorInput :message="formErrors.photo?.[0]" />
             </div>
-
-            <!-- NAME PRODUCT -->
+        </div>
+        <div
+            v-for="field in fields"
+            class="grid sm:grid-cols-12 mt-4 gap-2 sm:gap-6"
+        >
             <div class="sm:col-span-3">
-                <FormLabel for="name" label="Nome do Produto:" />
+                <FormLabel :for="field.id" :label="field.label" />
             </div>
             <div class="sm:col-span-9">
-                <FormTextInput
-                    id="name"
-                    v-model="form.name"
-                    placeholder="Informe o nome do produto..."
+                <Component
+                    :is="field.component"
+                    v-bind="field.bindings"
+                    v-model="form[field.id]"
+                    :placeholder="field.placeholder"
                 />
-                <FormErrorInput :message="formErrors.name?.[0]" />
-            </div>
-
-            <!-- DESCRIPTION PRODUCT -->
-            <div class="sm:col-span-3">
-                <FormLabel for="description" label="Descrição do produto:" />
-            </div>
-            <div class="sm:col-span-9">
-                <FormTextArea
-                    id="description"
-                    v-model="form.description"
-                    placeholder="Informe uma descrição detalhada do produto..."
-                />
-                <FormErrorInput :message="formErrors.description?.[0]" />
-            </div>
-
-            <!-- PRICE PRODUCT -->
-            <div class="sm:col-span-3">
-                <FormLabel for="price" label="Preço:" />
-            </div>
-            <div class="sm:col-span-9">
-                <FormCurrencyInput
-                    id="price"
-                    v-model="form.price"
-                    placeholder="Informe o preço do produto(ex:R$ 100,00)..."
-                />
-                <FormErrorInput :message="formErrors.price?.[0]" />
-            </div>
-
-            <!-- PRODUCT DISCOUNT -->
-            <div class="sm:col-span-3">
-                <div class="inline-block">
-                    <FormLabel for="discount" label="Desconto:" />
-                    <span class="text-sm text-gray-400"
-                        >(Em porcentagem %)</span
-                    >
-                </div>
-            </div>
-            <div class="sm:col-span-9">
-                <FormPercentInput
-                    id="discount"
-                    v-model="form.discount"
-                    placeholder="Informe a % de desconto(ex:10.00 para 10%)..."
-                />
-                <FormErrorInput :message="formErrors.discount?.[0]" />
-            </div>
-
-            <!-- QUANTITY OF PRODUCT IN STOCK -->
-            <div class="sm:col-span-3">
-                <FormLabel for="quantity" label="Quantidade em estoque:" />
-            </div>
-            <div class="sm:col-span-9">
-                <FormNumberInput
-                    id="quantity"
-                    v-model="form.quantity"
-                    placeholder="Informe a quantidade disponível em estoque..."
-                />
-                <FormErrorInput :message="formErrors.quantity?.[0]" />
+                <FormErrorInput :message="formErrors[field.id]?.[0]" />
             </div>
         </div>
 
         <div class="mt-4 flex justify-end gap-x-4">
             <button
+                v-if="!isEditing"
                 type="button"
                 @click="resetForm"
                 class="py-[5px] px-2 inline-flex justify-center items-center gap-2 rounded-lg font-medium bg-gray-100 text-gray-700 hover:bg-gray-200 transition-all text-sm"
@@ -101,7 +48,7 @@
                 type="submit"
                 class="py-1.5 px-2 inline-flex justify-center items-center gap-2 rounded-lg border border-indigo-500 font-medium bg-indigo-100 text-indigo-700 hover:bg-indigo-200 transition-all text-sm"
             >
-                Criar Produto
+                {{ isEditing ? "Atualizar Produto" : "Criar Produto" }}
             </button>
         </div>
     </form>
@@ -141,6 +88,14 @@ const form = ref({
     quantity: "",
 });
 
+const fields = [
+    {id: "name",label: "Nome do Produto:",component: FormTextInput,bindings: { id: "name" },placeholder: "Informe o nome do produto...",},
+    {id: "description",label: "Descrição do Produto:",component: FormTextArea,bindings: { id: "description" },placeholder: "Informe uma descrição detalhada do produto...",},
+    {id: "price",label: "Preço:",component: FormCurrencyInput,bindings: { id: "price" },placeholder: "Informe o preço do produto(ex:R$ 100,00)...",},
+    {id: "discount",label: "Desconto:(Em porcentagem %)",component: FormPercentInput,bindings: { id: "discount" },placeholder: "Informe a % de desconto(ex:10.00 para 10%)...",},
+    {id: "quantity",label: "Quantidade em estoque:",component: FormNumberInput,bindings: { id: "quantity" },placeholder: "Informe a quantidade disponível em estoque...",},
+];
+
 watch(
     () => props.product,
     (newProduct) => {
@@ -168,19 +123,19 @@ const resetForm = () => {
 
 const createOrUpdateProduct = async () => {
     try {
-        formErrors.value = {};  
-        if (isEditing.value) {            
-            const response = await Services.products.update(form.value.id, form.value);
-            toast.success(response.message);
-        }else{
-            const response = await Services.products.create(form.value);
-            toast.success(response.message);
-            resetForm();
-        }
+        formErrors.value = {};
+        const response = isEditing.value
+            ? await Services.products.update(form.value.id, form.value)
+            : await Services.products.create(form.value);
+
+        toast.success(response.message);
+        setTimeout(() => {
+            window.location.href = `/products/${
+                isEditing.value ? form.value.id : response.data.id
+            }`;
+        }, 2500);
     } catch (error) {
-        if (error) {
-            formErrors.value = error;
-        }
+        formErrors.value = error;
         errorMessage.value = error.message?.[0];
     }
 };
